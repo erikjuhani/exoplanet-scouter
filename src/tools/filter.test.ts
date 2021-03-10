c5817240a229f156993c8b5efe77651a5a3524eb
erikjuhani <erik.kinn@gmail.com>
2021-03-10 13:21:23 +0200

    Add filter tool
    Filter is used to filter out unwanted planet results.
    Currently only two filtering criteria are accepted (Only non
    controversial or potentially habitable).

---
+++ src/tools/filter.test.ts
@@ -0,0 +1,26 @@
+import {
+  proximaCenB,
+  gj411b,
+  tauCetg,
+  hd219134f,
+} from "../models/__mocks__/planets";
+import { Planet } from "../types";
+import { createFilter, FilterCriteria } from "./filter";
+
+describe("Filter", () => {
+  const planets = [proximaCenB, gj411b, tauCetg, hd219134f];
+
+  test.each`
+    criteria                               | negate   | expected
+    ${FilterCriteria.PotentiallyHabitable} | ${false} | ${[proximaCenB]}
+    ${FilterCriteria.OnlyNonContoversial}  | ${true}  | ${[proximaCenB, gj411b, tauCetg]}
+  `("evaluate", ({ criteria, negate, expected }) => {
+    let actual: Planet[] = [];
+    if (negate) {
+      actual = planets.filter(createFilter(criteria).negate().filter);
+    } else {
+      actual = planets.filter(createFilter(criteria).filter);
+    }
+    expect(actual).toEqual(expect.arrayContaining(expected));
+  });
+});
---
+++ src/tools/filter.ts
@@ -0,0 +1,46 @@
+import { Planet } from "../types";
+
+export enum FilterCriteria {
+  OnlyNonContoversial = "ONLY_NON_CONTROVERSIAL",
+  PotentiallyHabitable = "POTENTIALLY_HABITABLE",
+}
+
+export type Filter = {
+  criteria?: FilterCriteria;
+  negation: 0 | 1;
+  negate: () => Filter;
+  filter: (planet: Planet) => boolean;
+};
+
+export const createFilter = (criteria?: FilterCriteria): Filter => {
+  const filter: Filter = {
+    criteria,
+    negation: 0,
+    negate: () => {
+      filter.negation = filter.negation ? 0 : 1;
+      return filter;
+    },
+    filter: (planet: Planet) => {
+      let key: keyof Planet | undefined = undefined;
+
+      switch (filter.criteria) {
+        case FilterCriteria.PotentiallyHabitable:
+          key = "potentiallyHabitable";
+          break;
+        case FilterCriteria.OnlyNonContoversial:
+          key = "controversial";
+          break;
+      }
+
+      if (key) {
+        return nand(filter.negation, planet[key]);
+      }
+
+      return true;
+    },
+  };
+
+  return filter;
+};
+
+const nand = <T>(a: 0 | 1, b: T): boolean => (a ? !b : !!b);
