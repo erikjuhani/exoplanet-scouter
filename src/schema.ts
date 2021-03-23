638cbce8a23d7649312d11aa94c4f1b545edc0c8
erikjuhani <erik.kinn@gmail.com>
2021-03-23 08:10:00 +0200

    Add field descriptions

--- src/schema.ts
+++ src/schema.ts
@@ -45,28 +45,66 @@ const order = `
 `;
 
 const exoplanet = `
+  "Exoplanet is a planet outside of our Solar system."
   type Exoplanet {
+    "Most commonly used name in literature."
     name: String!
+
+    "Year the planet was discovered."
     yearOfDiscovery: Int!
+
+    "Boolean flag indicating whether the confirmation status of a planet has been questioned in the published literature."
     controversial: Boolean!
+
+    "Date of last update to planet parameters."
     updatedAt: String!
+
+	  """
+    Distance to the planetary system in units of parsecs.
+
+    Change returned distance unit by giving a DistanceUnit parameter.
+    """
     distance(unit: DistanceUnit = PARSEC): Float
+
+    """
+    The longest radius of an elliptic orbit. Measured in astronimical units (AU).
+    """
     orbitSemiMajorAxis: Float
+
+    """
+    Amount of matter contained in the planet. Measured in units of masses of Jupiter.
+    """
     mass: Float
+
+    """
+    Amount of mass per unit of volume of the planet. (g/cm**3)
+    """
     density: Float
+
+    """
+    Planet exists in the habitable zone, which might make it potentially habitable for life.
+    """
     potentiallyHabitable: Boolean!
+
+    "The host star of the planet."
     star: Star!
   }
 `;
 
 const latLng = `
+  """
+  Galactic coordinates system is depicted in spherical coordinates. The Sun as it's center.
+  """
   type LatLng {
+    "Galactic longitude (l) measured in degrees (0° to 360°)"
     longitude: Float!
+    "Galactic latitude () measured in degrees (-90° to 90°)"
     latitude: Float!
   }
 `;
 
 const xyz = `
+  "Galactic Coordinates in cartesian units."
   type XYZ {
     x: Float!
     y: Float!
@@ -77,16 +115,42 @@ const xyz = `
 const star = `
   union Coordinate = LatLng | XYZ
 
+  "A luminous astronomical object."
   type Star {
+    "Stellar name most commonly used in the literature."
     name: String!
+
+    "The amount of planets in the star system."
     orbitingPlanets: Int!
-    hdName: String
-    hipName: String
+
+	  "Distance to the planetary system in units of parsecs."
     distance: Float
+  
+    "Stellar age of the host star."
     age: Float
+
+    """
+    Amount of matter contained in the star, measured in units of masses of the Sun.
+    """
     mass: Float
+
+    """
+    A length of a line from the centre of the star to it's surface. Measured in units of radius of the Sun.
+    """
     radius: Float
+
+    """
+    Coordinates in Galactic Coordinate system.
+
+    Change returned Coordinate by giving a CoordinateUnit parameter.
+    """
     coordinates(unit: CoordinateUnit = LAT_LNG): Coordinate
+
+    """
+    Temperature of the star as modeled by a black body emitting the same total amount of electromagnetic radiation.
+    
+    Change returned temperature unit by giving a TemperatureUnit parameter.
+    """
     temperature(unit: TemperatureUnit = KELVIN): Float
   }
 `;
@@ -100,6 +164,12 @@ const exoplanetQuery = `
 `;
 
 export const typeDefs = gql`
+  """
+  Field descriptions are referenced from here:
+  https://exoplanetarchive.ipac.caltech.edu/docs/API_exoplanet_columns.html
+
+  Additionally more information can be retrieved from the introduced link.
+  """
   ${distanceUnit}
   ${temperatureUnit}
   ${coordinateUnit}
